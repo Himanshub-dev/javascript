fbf3fa7634198300bb90ba914dac466470ec83b9
Himanshu Shekhar <85550997+Himanshub-dev@users.noreply.github.com>
2023-07-25 05:14:05 +0000

    variables

---
+++ basics_js/1_variable.js
@@ -0,0 +1,6 @@
+const accountId= 12345
+let accountEmail = "himanshu@hmail.com"
+var accountPassword = "13072002"
+accountCity = "Siwan"
+let accountState;
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
