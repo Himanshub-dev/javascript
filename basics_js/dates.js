2c5ea1100ce7cd44860af096fe5ba9f0221ef651
Himanshu Shekhar <85550997+Himanshub-dev@users.noreply.github.com>
2023-07-26 06:03:26 +0000

    dates

---
+++ basics_js/dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+
+
+let mycreateddate = new Date(2023, 0, 25)
+console.log(mycreateddate.toDateString());
+
+
+
+let mytimestamp= Date.now()
+console.log(mytimestamp);
